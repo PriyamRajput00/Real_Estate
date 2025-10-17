e8fbc91b0a3338fb95273917064f99b10592a863
Priyam Rajput <priyamrajput99@gmail.com>
2025-10-17 19:19:37 +0530

    create get user listing api route

--- api/routes/listing.route.js
+++ api/routes/listing.route.js
@@ -5,5 +5,8 @@ import { verifyToken } from '../utils/verifyUser.js';
 const router = express.Router();
 
 router.post('/create', verifyToken, createListing)
+router.post('/test', (req, res) => {
+    res.json({ message: "Test route works!" });
+});
 
 export default router;
